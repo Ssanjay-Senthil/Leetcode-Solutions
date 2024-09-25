d3e65466129101d003be519b07bdf29f3ab612ee
Ssanjay-Senthil <125140959+Ssanjay-Senthil@users.noreply.github.com>
2024-09-25 15:31:43 +0530

    Time: 7 ms (83.29%), Space: 42.4 MB (55.49%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -2,11 +2,13 @@ class Solution {
     public boolean isIsomorphic(String s, String t) {
         int a[] = new int[256];
         int b[] = new int[256];
+        int k=256;
         for(int i=0;i<s.length();i++){
             if(a[s.charAt(i)] != b[t.charAt(i)])
             return false;
-            a[s.charAt(i)] = i+1;
-            b[t.charAt(i)] = i+1;
+            a[s.charAt(i)] = k-1; //if we make a[s.charAt(i)]++ , then it checks only frequency of letters same or which may result in logical error 
+            b[t.charAt(i)] = k-1;
+            k--;
         }
         return true;
     }
