804ac5c11a7fd85a647be555629298cd461f7c84
Ssanjay-Senthil <125140959+Ssanjay-Senthil@users.noreply.github.com>
2024-09-26 14:48:27 +0530

    Time: 2 ms (84.19%), Space: 41.7 MB (54.41%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -1,36 +1,13 @@
 class Solution {
     public int longestPalindrome(String s) {
-        int[] a = new int[52];
+        int[] a = new int[58];
         for(int i=0;i<s.length();i++){
-            char c = s.charAt(i);
-            if(c>=65 && c<=90)
-            a[c-'A']++;
-            else
-            a[c-'a'+26]++;
+            a[s.charAt(i)-'A']++;
         } 
         int ans=0;
-        int flag=0;
-        int flag1=0;
-        System.out.print(Arrays.toString(a));
-        for(int i=0;i<52;i++){
-            if(a[i]%2==0)
-            ans+=a[i];
-            else if(a[i]%2==1){
-                if(flag==0 && flag1==0 && a[i]==1){
-                    ans+=1;
-                    flag=1;
-                    flag1=1;
-                }
-                else if(a[i]>2){
-                    if(flag1==1)
-                    ans+=a[i]-1;
-                    else{
-                        ans+=a[i];
-                        flag1=1;
-                    }
-                }
-            }
+        for(int i=0;i<58;i++){
+            ans+=a[i]%2==0?a[i]:a[i]-1;
         }
-        return ans;
+        return ans==s.length()?ans:ans+1;
     }
 }
