8fb0fb21dc260daa85bd41205f4a3195bee257ed
Ssanjay-Senthil <125140959+Ssanjay-Senthil@users.noreply.github.com>
2024-10-22 14:29:39 +0530

    Time: 0 ms (100%), Space: 40.8 MB (86.6%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
@@ -8,18 +8,30 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode middleNode(ListNode head) {
+//         ListNode temp = head;
+//         int n=0;
+//         while(temp!=null){
+//             temp = temp.next;
+//             n++;
+//         }
+//         int middle = n/2;
+//         for(int i=0;i<middle;i++){
+//             head=head.next;
+//         }
+//         return head;
+//     }
+// }
+// Using Two Pointers
 class Solution {
-    public ListNode middleNode(ListNode head) {
-        ListNode temp = head;
-        int n=0;
-        while(temp!=null){
-            temp = temp.next;
-            n++;
-        }
-        int middle = n/2;
-        for(int i=0;i<middle;i++){
-            head=head.next;
-        }
-        return head;
+   public ListNode middleNode(ListNode head) {
+    ListNode slow=head;
+    ListNode fast=head;
+    while(fast!=null && fast.next!=null){
+        slow=slow.next;
+        fast=fast.next.next;
     }
+    return slow;
+   }
 }
