4df7e7deb0853dc0ae0821508ee53167d3bc3268
Sanjay <sanjays.22eee@kongu.edu>
2025-04-30 14:34:33 +0530

    Time: 2 ms (94.05%), Space: 55.7 MB (76.85%) - LeetHub

---
+++ 0890-lemonade-change/0890-lemonade-change.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int n = bills.length;
+        int arr[] = new int[3];
+        for(int i=0;i<n;i++){
+            if(bills[i]==5) arr[0]++;
+            else if(bills[i]==10){
+                if(arr[0]>0) 
+                {
+                    arr[1]++; 
+                    arr[0]--;
+                }
+            else return false;
+            }
+            else{
+                if(bills[i] == 20 && arr[0]>0 && arr[1]>0) 
+                {
+                    arr[0]--;
+                    arr[1]--;
+                }
+                else if(bills[i] == 20 && arr[0]>2){
+                    arr[0]-=3;
+                }
+                else return false;
+            }
+        }
+        return true;
+    }
+}
